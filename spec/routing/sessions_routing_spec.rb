9c0ad052aaf9199fdbb80b46a4bf6da8814fdb0d
Vladislav Petrov <v.petrov@agilefusion.com>
2015-04-15 01:53:39 +0300

    Add routing test for sessions controller

---
+++ spec/routing/sessions_routing_spec.rb
@@ -0,0 +1,5 @@
+describe SessionsController do
+  it { expect(get: '/login').to route_to('sessions#new') }
+  it { expect(post: '/login').to route_to('sessions#create') }
+  it { expect(delete: '/logout').to route_to('sessions#destroy') }
+end
