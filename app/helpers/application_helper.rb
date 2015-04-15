5b2e2ce5980c8fe0460d9b2c1ebe512044147948
Vladislav Petrov <v.petrov@agilefusion.com>
2015-04-15 23:23:04 +0300

    Rebuild sign in form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     return User.new unless session[:user_id]
     @_current_user ||= User.find(session[:user_id])
   end
+
+  def signed_in?
+    defined? @_current_user
+  end
 end
