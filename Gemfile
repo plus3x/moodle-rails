8aab8780b825b420354293f5a598677d66c9b940
Vladislav Petrov <v.petrov@agilefusion.com>
2015-04-15 20:39:35 +0300

    Add simple form

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3'
+gem 'simple_form'
 
 group :test do
   gem 'factory_girl_rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+users = [
+  { role: 'student', login: 'student', email: 'student@mail.com', password: 'student' },
+  { role: 'teacher', login: 'teacher', email: 'teacher@mail.com', password: 'teacher' },
+  { role: 'manager', login: 'manager', email: 'manager@mail.com', password: 'manager' },
+  { role: 'admin', login: 'admin', email: 'admin@mail.com', password: 'admin' }
+]
+puts 'Default users:'
+User.create!(users).each do |user|
+  puts "  #{user.id} - #{user.login}"
+end
+
+Course.delete_all
+courses = [
+  { title: 'New course' }
+]
+puts 'Default courses:'
+Course.create!(courses).each do |course|
+  puts "  #{course.id} - #{course.title}"
+end
+
+LearningUnit.delete_all
+learning_units = [
+  { title: 'New learning unit', course: Course.first }
+]
+puts 'Default learning units:'
+LearningUnit.create!(learning_units).each do |learning_unit|
+  puts "  #{learning_unit.id} - #{learning_unit.title} on course \"#{learning_unit.course.title}\""
+end
+
+Activity.delete_all
+activities = [
+  { title: 'New activity', learning_unit: LearningUnit.first }
+]
+puts 'Default activities:'
+Activity.create!(activities).each do |activity|
+  puts "  #{activity.id} - #{activity.title} on learning unit \"#{activity.learning_unit.title}\""
+end
