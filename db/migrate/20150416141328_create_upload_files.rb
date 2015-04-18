2d1e011f0fa07891ac5dad8bec1d68f63bb0c86a
Vladislav Petrov <v.petrov@agilefusion.com>
2015-04-19 02:57:56 +0300

    Fix migrations for postgres

--- db/migrate/20150416141328_create_upload_files.rb
+++ db/migrate/20150416141328_create_upload_files.rb
@@ -1,8 +1,9 @@
 class CreateUploadFiles < ActiveRecord::Migration
   def change
     create_table :upload_files do |t|
-      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :author, index: true
       t.belongs_to :activity, index: true, foreign_key: true
+      t.belongs_to :submission, index: true
       t.string :file, index: true, null: false
 
       t.timestamps null: false
--- db/migrate/20150417102446_create_submissions.rb
+++ db/migrate/20150417102446_create_submissions.rb
@@ -1,14 +1,13 @@
 class CreateSubmissions < ActiveRecord::Migration
   def change
     create_table :submissions do |t|
-      t.belongs_to :files, index: true, foreign_key: true
-      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :files, index: true
+      t.belongs_to :author, index: true
       t.belongs_to :activity, index: true, foreign_key: true
       t.string :comment, null: false
       t.integer :grade, default: 0
 
       t.timestamps null: false
     end
-    add_belongs_to :upload_files, :submission, index: true, foreign_key: true
   end
 end
